b6470f4344c244fc6e556da2bb82410db08b278f
Manish Kumar <66010479+manishk-99@users.noreply.github.com>
2021-03-24 16:15:50 +0530

    Axios used

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import "./styles.css";
+import React, { useState, useEffect } from "react";
+import { Container, Row, Col } from "reactstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
-}
+import Axios from "axios";
+
+const App = () => {
+  const [details, setDetails] = useState({});
+
+  const fetchDetails = async () => {
+    const { data } = await Axios.get("https://randomuser.me/api/");
+    // console.log("Response: ", response);
+
+    const details = data.results[0];
+
+    setDetails(details);
+  };
+
+  useEffect(() => {
+    fetchDetails();
+  }, []);
+
+  return <h1>Hiiii</h1>;
+};
+export default App;
